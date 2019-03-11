011010824c12d00e50e6a18340ea5189e17b5ef0
root <root@packer-debian-8-amd64.droplet.local>
2019-03-11 12:17:51 +0000

    giann_fr namespace

--- app.lua
+++ app.lua
@@ -2,10 +2,12 @@ local lapis = require("lapis")
 
 local app = lapis.Application()
 app:enable("etlua")
-app.layout = require "views.layout"
+app.layout = require "giann_fr.views.layout"
+app.views_prefix = "giann_fr.views"
+app.flows_prefix = "giann_fr.flows"
 
 app:get("/", function(self)
-    return { render = "index" }
+    return { render = "index" } -- what is this string documented as?
 end)
 
 return app
