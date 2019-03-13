1d7197e87f2804d5675b16eda4a72eaf3189cdc7
Benoit Giannangeli <giann008@gmail.com>
2019-03-13 13:26:21 +0100

    Moon logo

--- app.lua
+++ app.lua
@@ -2,9 +2,9 @@ local lapis = require("lapis")
 
 local app = lapis.Application()
 app:enable("etlua")
-app.layout = require "views.layout"
-app.views_prefix = "views"
-app.flows_prefix = "flows"
+app.layout = require "giann_fr.views.layout"
+app.views_prefix = "giann_fr.views"
+app.flows_prefix = "giann_fr.flows"
 
 app:get("/", function(self)
     return { render = "index" } -- what is this string documented as?
